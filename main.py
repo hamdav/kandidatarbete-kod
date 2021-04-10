d89ba705c1210246367b3a8fbc20ccc904a62800
David Hambraeus <Ddhambraeus@gmail.com>
2021-04-10 17:37:18 +0200

    Fix the new and improved S for chevally

--- main.py
+++ main.py
@@ -1,25 +1,56 @@
-from Polynomial import Polynomial, Term
-
 import time
+import math
+import itertools
 import numpy as np
 #from quicktions import Fraction
 
+from Polynomial import Polynomial, Term
 from structConsts import zeropols, cdict
 
-def generateS(d):
-    return Polynomial({(-i,i): 1 for i in range(1,d+1)})
+def neg(gm, n, r):
+    if gm < 0:
+        if gm < -n + r:
+            return gm
+        elif gm < (-n + r) // 2:
+            return gm + (-n + r) // 2
+        else:
+            return gm - (-n + r) // 2
+    if gm > 0:
+        if gm > n - r:
+            return gm
+        elif gm > (n - r) // 2:
+            return gm - (n - r) // 2
+        else:
+            return gm + (n - r) // 2
+    else:
+        return 0
 
+def generateS(n, r):
+    return Polynomial({(-i, neg(i, n, r)): 1 for i in range(1,n+1)})
 
+def generatePowerOfS(n, r, p, trueFactors=False):
+    # If trueFactors, returns the power of S with the actual factors, 
+    # which are very large for large p
+    # Otherwise returns S scaled so that the factor is 1 for all terms
+    termsInS = ((-i, neg(i, n, r)) for i in range(1, n+1))
+    return Polynomial([Term(tuple(itertools.chain.from_iterable(gms)), 
+                            math.factorial(p) if trueFactors else 1,
+                            gmsAreSorted=False)
+                       for gms in itertools.combinations(termsInS, p)])
 
 start = time.time()
-s = generateS(14)
-p = s*s
+p = generatePowerOfS(52, 4, 52)
+breakpoint()
 q = p.reduce3(cdict, zeropols)
+print(q)
+#s = generateS(15, 3)
+#p = s*s
+#q = p.reduce3(cdict, zeropols)
 #r = q.reduce3(cdict, zeropols)
 #print(len(r-q))
 end = time.time()
 print(f"Time duration: {end - start}")
 
-for key in zeropols:
-    if key[1] == 'UU':
-        print(zeropols[key])
+#for key in zeropols:
+    #if key[1] == 'UU':
+        #print(zeropols[key])
